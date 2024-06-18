a45773c199ab333deab7a79f6459758d08b914af
padmalatha k <padmalathatce@gmail.com>
2024-06-17 17:20:57 -0700

    Database connection

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-import app from './src/app';
+import app from "./src/app";
+import { connectToDB } from "./src/config/mongoose"; // Import the mongoose configuration file
 
-import { connectToDB } from './src/config/mongoose'; // Import the mongoose configuration file
-
-connectToDB()
+connectToDB();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
